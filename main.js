5873508e4ac7162433a67a50bda4e3180df8e284
OlgaLosikova <techtigra@gmail.com>
2024-06-22 18:27:57 +0300

    fix

--- main.js
+++ main.js
@@ -13,20 +13,19 @@ const nextButton = document.getElementById('next');
 //Шапку таблицы добавляем первым элементом таблицы всякий раз при рендеринге
 const tableHeader = document.getElementById('tableHeader').innerHTML;
 
-//Индекс удаляемого элемента
-let stringId;
-
 //Поле поиска, кнопка очистки фильтрации, пустой массив поиска
 const searchInput = document.querySelector('.search');
 const cleanButton = document.querySelector('.clean-button');
-let filterUsers = [];
+let resultUsers = [];
+let isEmptySearch = false;
 
-//Кнопки Да и Нет в модальном окне
+//Кнопки Да и Нет в модальном окне, индекс удаляемого элемента
 const yesButton = document.getElementById('yesButton');
 const noButton = document.getElementById('noButton');
+let stringId;
+let decrementUsers = [];
 
-//Пустой массив сортировки, кнопки сортировки, переменная направления сортировки
-let sortUsers = [];
+//Кнопки сортировки, переменная направления сортировки
 const sortDateButton = document.querySelector('.filters__black');
 const sortRatingButton = document.querySelector('.filters__gray');
 let dateDirection = 0;
@@ -48,42 +47,44 @@ const main = async () => {
   const users = await fetchUsers();
 
   const renderingUsers = () => {
-    console.log('searchInput.value', searchInput.value);
-
     //При первой отрисовке записываем всех пришедших из api пользователей в массив, если есть запрос в поиске и/или удаленная строка, перезаписываем массив
-    filterUsers = (filterUsers.length === 0 ? users : filterUsers)
-      .filter((user) => {
-        if (user.id !== stringId) {
-          return true;
-        }
-        return false;
-      })
-      .filter((user) => {
-        if (
-          user.username.toLowerCase().includes(searchInput.value.toLowerCase()) ||
-          user.email.toLowerCase().includes(searchInput.value.toLowerCase())
-        ) {
-          return true;
-        }
-        return false;
-      });
-    console.log('filterUsers', filterUsers);
-    if (dateDirection === 'DESC') {
-      sortUsers = filterUsers.sort((a, b) => (a.registration_date < b.registration_date ? 1 : -1));
-    } else if (dateDirection === 'ASC') {
-      sortUsers = filterUsers.sort((a, b) => (a.registration_date > b.registration_date ? 1 : -1));
-    } else sortUsers = filterUsers.sort((a, b) => (a.id > b.id ? 1 : -1));
+    console.log('------------------------');
 
-    console.log('sortUsers', sortUsers);
-    filterUsers.length >= 5 ? (footer.style.display = 'flex') : (footer.style.display = 'none');
-    console.log('itemIndex', itemIndex);
-    //Исходя из полученного на предыдущем шаге массива,записываем в массив отображаем первую страницу записей
-    const usersPage = filterUsers.slice(
-      itemIndex,
-      filterUsers[itemIndex + 5] ? itemIndex + 5 : filterUsers.length,
+    decrementUsers = (decrementUsers.length === 0 ? users : resultUsers).filter(
+      (user) => user.id !== stringId,
     );
-    console.log('usersPage', usersPage);
+    console.log('decrementUsers.length', decrementUsers.length);
+    resultUsers = (
+      resultUsers.length === 0
+        ? users
+        : decrementUsers.length < 25 && decrementUsers.length > 0
+        ? decrementUsers
+        : resultUsers
+    ).filter(
+      (user) =>
+        user.username.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchInput.value.toLowerCase()),
+    );
 
+    //Сортировка по дате или рейтингу
+    if (dateDirection === 'DESC') {
+      resultUsers.sort((a, b) => (a.registration_date < b.registration_date ? 1 : -1));
+    } else if (dateDirection === 'ASC') {
+      resultUsers.sort((a, b) => (a.registration_date > b.registration_date ? 1 : -1));
+    } else if (ratingDirection === 'DESC') {
+      resultUsers.sort((a, b) => (a.rating < b.rating ? 1 : -1));
+    } else if (ratingDirection === 'ASC') {
+      resultUsers.sort((a, b) => (a.rating > b.rating ? 1 : -1));
+    } else resultUsers.sort((a, b) => a.id - b.id);
+    console.log('сортировка и поиск в resultUsers', resultUsers);
+    resultUsers.length >= 5 ? (footer.style.display = 'flex') : (footer.style.display = 'none');
+
+    //Исходя из полученного на предыдущем шаге массива,записываем в массив отображаем первую страницу записей
+    const usersPage = resultUsers.slice(
+      itemIndex,
+      resultUsers[itemIndex + 5] ? itemIndex + 5 : resultUsers.length,
+    );
+    console.log('страница результатов c возможным удалением, поиском, фильтрацией', usersPage);
     resultTable.innerHTML = tableHeader;
     usersPage.length === 0
       ? (resultTable.innerHTML = `<span class="table_empty">Нет ни одной записи</span>`)
@@ -121,7 +122,7 @@ const main = async () => {
     pageIndex++;
     pageNumber.innerText = pageIndex;
     prevButton.disabled = false;
-    if (itemIndex >= filterUsers.length - 5) {
+    if (itemIndex >= resultUsers.length - 5) {
       nextButton.disabled = true;
     }
     renderingUsers();
@@ -134,7 +135,7 @@ const main = async () => {
     if (itemIndex === 0) {
       prevButton.disabled = true;
     }
-    if (itemIndex >= filterUsers.length - 5) {
+    if (itemIndex >= resultUsers.length - 5) {
       nextButton.disabled = true;
     }
     renderingUsers();
@@ -156,17 +157,17 @@ const main = async () => {
   };
   resultTable.addEventListener('click', deleteString);
 
-  //Поиск
+  //Сброс поиска и сортировки
   const searchClean = () => {
-    searchInput.value !== 0 && (searchInput.value = '');
-    filterUsers = [];
+    searchInput.value = '';
+    resultUsers = decrementUsers;
     dateDirection = 0;
     ratingDirection = 0;
-    console.log(filterUsers);
     goToFirstPage();
     renderingUsers();
     cleanButton.style.display = 'none';
   };
+  cleanButton.addEventListener('click', searchClean);
   //При запуске поиска и сбросе запроса результаты начинаем отображать с первой страницы
   const goToFirstPage = () => {
     itemIndex = 0;
@@ -175,7 +176,7 @@ const main = async () => {
     prevButton.disabled = true;
     nextButton.disabled = false;
   };
-  cleanButton.addEventListener('click', searchClean);
+  // Поиск
   const searchUsers = () => {
     if (searchInput.value !== '' && searchInput.value.length > 2) {
       goToFirstPage();
@@ -187,16 +188,29 @@ const main = async () => {
   };
   searchInput.addEventListener('input', searchUsers);
 
-  //Сортировка
+  //Сортировка по дате
   const sortDate = () => {
+    ratingDirection = 0;
+    cleanButton.style.display = 'flex';
     dateDirection === 0
       ? (dateDirection = 'DESC')
       : dateDirection === 'DESC'
       ? (dateDirection = 'ASC')
       : dateDirection === 'ASC' && (dateDirection = 0);
-    console.log('dateDirection', dateDirection);
     renderingUsers();
   };
   sortDateButton.addEventListener('click', sortDate);
+  //Сортировка по рейтингу
+  const sortRating = () => {
+    dateDirection = 0;
+    cleanButton.style.display = 'flex';
+    ratingDirection === 0
+      ? (ratingDirection = 'DESC')
+      : ratingDirection === 'DESC'
+      ? (ratingDirection = 'ASC')
+      : ratingDirection === 'ASC' && (ratingDirection = 0);
+    renderingUsers();
+  };
+  sortRatingButton.addEventListener('click', sortRating);
 };
 main();
