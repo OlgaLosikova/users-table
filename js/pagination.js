cda7c6b526cfdcb2d49b9b26b1503d58d4d43312
OlgaLosikova <techtigra@gmail.com>
2024-06-22 14:52:03 +0300

    sorting

--- js/pagination.js
+++
@@ -1,9 +0,0 @@
-export function pageNext(element) {
-  let index = 0;
-  const setIndex = (index) => {
-    counter = index;
-    console.log(counter);
-  };
-  element.addEventListener('click', () => setIndex(counter + 5));
-  setCounter(0);
-}
--- main.js
+++ main.js
@@ -16,9 +16,21 @@ const tableHeader = document.getElementById('tableHeader').innerHTML;
 //Индекс удаляемого элемента
 let stringId;
 
-//Поле поиска, кнопка очистки фильтрации
+//Поле поиска, кнопка очистки фильтрации, пустой массив поиска
 const searchInput = document.querySelector('.search');
 const cleanButton = document.querySelector('.clean-button');
+let filterUsers = [];
+
+//Кнопки Да и Нет в модальном окне
+const yesButton = document.getElementById('yesButton');
+const noButton = document.getElementById('noButton');
+
+//Пустой массив сортировки, кнопки сортировки, переменная направления сортировки
+let sortUsers = [];
+const sortDateButton = document.querySelector('.filters__black');
+const sortRatingButton = document.querySelector('.filters__gray');
+let dateDirection = 0;
+let ratingDirection = 0;
 
 const fetchUsers = async () => {
   try {
@@ -36,7 +48,10 @@ const main = async () => {
   const users = await fetchUsers();
 
   const renderingUsers = () => {
-    const filterUsers = users
+    console.log('searchInput.value', searchInput.value);
+
+    //При первой отрисовке записываем всех пришедших из api пользователей в массив, если есть запрос в поиске и/или удаленная строка, перезаписываем массив
+    filterUsers = (filterUsers.length === 0 ? users : filterUsers)
       .filter((user) => {
         if (user.id !== stringId) {
           return true;
@@ -53,15 +68,29 @@ const main = async () => {
         return false;
       });
     console.log('filterUsers', filterUsers);
+    if (dateDirection === 'DESC') {
+      sortUsers = filterUsers.sort((a, b) => (a.registration_date < b.registration_date ? 1 : -1));
+    } else if (dateDirection === 'ASC') {
+      sortUsers = filterUsers.sort((a, b) => (a.registration_date > b.registration_date ? 1 : -1));
+    } else sortUsers = filterUsers.sort((a, b) => (a.id > b.id ? 1 : -1));
+
+    console.log('sortUsers', sortUsers);
     filterUsers.length >= 5 ? (footer.style.display = 'flex') : (footer.style.display = 'none');
-    const usersPage = filterUsers.slice(itemIndex, itemIndex + 5);
+    console.log('itemIndex', itemIndex);
+    //Исходя из полученного на предыдущем шаге массива,записываем в массив отображаем первую страницу записей
+    const usersPage = filterUsers.slice(
+      itemIndex,
+      filterUsers[itemIndex + 5] ? itemIndex + 5 : filterUsers.length,
+    );
     console.log('usersPage', usersPage);
 
     resultTable.innerHTML = tableHeader;
-    usersPage.forEach((item) => {
-      const tableRow = document.createElement('div');
-      tableRow.classList.add('table_row');
-      tableRow.innerHTML = `<div class="table_data table_data__blue">${item.username}</div>
+    usersPage.length === 0
+      ? (resultTable.innerHTML = `<span class="table_empty">Нет ни одной записи</span>`)
+      : usersPage.forEach((item) => {
+          const tableRow = document.createElement('div');
+          tableRow.classList.add('table_row');
+          tableRow.innerHTML = `<div class="table_data table_data__blue">${item.username}</div>
         <div class="table_data">${item.email}</div>
         <div class="table_data">
           ${
@@ -69,10 +98,10 @@ const main = async () => {
               ? `0${new Date(item.registration_date).getDate()}`
               : new Date(item.registration_date).getDate()
           }.${
-        new Date(item.registration_date).getMonth() + 1 < 10
-          ? `0${new Date(item.registration_date).getMonth() + 1}`
-          : new Date(item.registration_date).getMonth() + 1
-      }.${new Date(item.registration_date).getFullYear()}
+            new Date(item.registration_date).getMonth() + 1 < 10
+              ? `0${new Date(item.registration_date).getMonth() + 1}`
+              : new Date(item.registration_date).getMonth() + 1
+          }.${new Date(item.registration_date).getFullYear()}
         </div>
         <div class="table_data">${item.rating}</div>
         <img
@@ -81,8 +110,8 @@ const main = async () => {
           src="./assets/svg/cancel.svg"
           alt="delete"
         />`;
-      resultTable.appendChild(tableRow);
-    });
+          resultTable.appendChild(tableRow);
+        });
   };
   renderingUsers();
 
@@ -92,7 +121,7 @@ const main = async () => {
     pageIndex++;
     pageNumber.innerText = pageIndex;
     prevButton.disabled = false;
-    if (itemIndex === users.length - 5) {
+    if (itemIndex >= filterUsers.length - 5) {
       nextButton.disabled = true;
     }
     renderingUsers();
@@ -105,6 +134,9 @@ const main = async () => {
     if (itemIndex === 0) {
       prevButton.disabled = true;
     }
+    if (itemIndex >= filterUsers.length - 5) {
+      nextButton.disabled = true;
+    }
     renderingUsers();
   });
 
@@ -113,10 +145,10 @@ const main = async () => {
     let removeBtn = e.target;
     removeBtn.tagName === 'IMG' && (modal.style.display = 'flex');
 
-    document.getElementById('noButton').addEventListener('click', function () {
+    noButton.addEventListener('click', function () {
       modal.style.display = 'none';
     });
-    document.getElementById('yesButton').addEventListener('click', function () {
+    yesButton.addEventListener('click', function () {
       modal.style.display = 'none';
       stringId = removeBtn.id;
       renderingUsers();
@@ -127,18 +159,44 @@ const main = async () => {
   //Поиск
   const searchClean = () => {
     searchInput.value !== 0 && (searchInput.value = '');
+    filterUsers = [];
+    dateDirection = 0;
+    ratingDirection = 0;
+    console.log(filterUsers);
+    goToFirstPage();
     renderingUsers();
-    document.querySelector('.clean-button').style.display = 'none';
+    cleanButton.style.display = 'none';
+  };
+  //При запуске поиска и сбросе запроса результаты начинаем отображать с первой страницы
+  const goToFirstPage = () => {
+    itemIndex = 0;
+    pageIndex = 1;
+    pageNumber.innerText = pageIndex;
+    prevButton.disabled = true;
+    nextButton.disabled = false;
   };
   cleanButton.addEventListener('click', searchClean);
   const searchUsers = () => {
     if (searchInput.value !== '' && searchInput.value.length > 2) {
+      goToFirstPage();
       renderingUsers();
-      document.querySelector('.clean-button').style.display = 'flex';
+      cleanButton.style.display = 'flex';
     } else if (searchInput.value === '') {
       searchClean();
     }
   };
   searchInput.addEventListener('input', searchUsers);
+
+  //Сортировка
+  const sortDate = () => {
+    dateDirection === 0
+      ? (dateDirection = 'DESC')
+      : dateDirection === 'DESC'
+      ? (dateDirection = 'ASC')
+      : dateDirection === 'ASC' && (dateDirection = 0);
+    console.log('dateDirection', dateDirection);
+    renderingUsers();
+  };
+  sortDateButton.addEventListener('click', sortDate);
 };
 main();
